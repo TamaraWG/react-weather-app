fb7819efa9056ad8882a4b6369611fb540742716
TamaraWG <walkertamara32@gmail.com>
2023-09-01 17:18:12 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div classname="container">
-        <h1>Weather App </h1>
-        <weather />
+        <Weather />
         <footer>
           This project was coded by Tamara Walker and is{" "}
           <a
