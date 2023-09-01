1f566a70a0facccf9adbc43e20d32981bc81049a
TamaraWG <walkertamara32@gmail.com>
2023-09-01 17:10:23 -0500

    added a container

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        This project was coded by Tamara Walker and is{" "}
-        <a
-          href="https://github.com/TamaraWG/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open-source code on GitHub and hosted on Netlify
-        </a>
-      </footer>
+      <div classname="container">
+        <h1>Weather App </h1>
+        <weather />
+        <footer>
+          This project was coded by Tamara Walker and is{" "}
+          <a
+            href="https://github.com/TamaraWG/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code on GitHub and hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
